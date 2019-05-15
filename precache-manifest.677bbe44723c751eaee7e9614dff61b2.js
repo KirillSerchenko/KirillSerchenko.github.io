d6f02342ce275e6cc684c9e7770400a093b45e2c
Kiril Serchenko <kirils@amdocs.com>
2019-05-15 14:58:49 +0300

    Updates

---
+++ precache-manifest.677bbe44723c751eaee7e9614dff61b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef590fd567af55a8759222610606e14a",
+    "url": "/static/media/bcg.ef590fd5.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3e2ffe80c8f00e819ef5",
+    "url": "/static/js/main.3e2ffe80.chunk.js"
+  },
+  {
+    "revision": "b49a0b7a7278ca3ad731",
+    "url": "/static/js/2.b49a0b7a.chunk.js"
+  },
+  {
+    "revision": "3e2ffe80c8f00e819ef5",
+    "url": "/static/css/main.986cbad0.chunk.css"
+  },
+  {
+    "revision": "32e14b72467a47491336c0589c6b0d6c",
+    "url": "/index.html"
+  }
+];
